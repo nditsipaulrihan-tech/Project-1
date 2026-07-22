87cbe3cbf3dbda505e47d1cd85bc29f698d90331
Paul Rihan <nditsipaulrihan@gmail.com>
2026-07-22 23:27:43 +0100

    yep

--- pages/send.php
+++ pages/send.php
@@ -9,19 +9,52 @@
 // Exit to see the output before mail() attempts to send
 // exit(); // You can uncomment this to stop script execution here
 
-$name = $_POST['name']?? '';
-$email = $_POST['email']?? '';
+$name = $email = '';
 $subject = "Clarity Advices";
+
+if(!($_SERVER['REQUEST_METHOD']=='POST')){
+
+}
+else{
+
+    if(empty($_POST['name'])){
+        echo "Input Name<br>";
+    }else{
+        // $name = sanitize($_POST['name'])
+        $name = trim($_POST['name']);
+        $name = stripslashes($_POST['name']);
+        $name = htmlspecialchars($_POST['name']);
+    } if(!preg_match("/^[a-zA-Z-' ]*$/",$name)){
+        echo "Invalid Name<br>";
+    }
+
+    if(empty($_POST['email'])){
+        echo "Required Email<br>";
+    } else{
+        $email = trim($_POST['email']);
+        $email = stripslashes($_POST['email']);
+        $email = htmlspecialchars($_POST['email']);
+    } if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "Invalid Email<br>";
+    }
+
+    // function sanitize($data){
+    //     $data = trim($data);
+    //     $data = stripslashes($data);
+    //     $data = htmlspecialchars($data);
+    //     return $data;
+    // }
 
 $to = "sexymosquito08@gmail.com";
 $from = $subject;
-$headers = "From: $from\r\nReply-To: $email\r\n";
 $body = "Name: $name\nEmail: $email";
+$headers = "From: $from\r\nReply-To: $email\r\n";
 
-if(mail($to, $body, $headers)){
-    echo "<b>Get Ready For Some Calrity!</b>
-    Go back to <a href='../index.html'>Home Page</a>";
+if(mail($to,$subject, $body, $headers)){
+    header("Location: ../index.html");
+    echo "<script>alert('Mail Sent, Get Ready for some clarity');</script>";
+    exit;
 } else {
-    echo "Error: Mail could not be sent. Check C:/xampp/sendmail/error.log";
-}
+    echo "Error: Mail could not be sent. ";
+}}
 ?>
--- pages/send_mail.php
+++ pages/send_mail.php
@@ -9,10 +9,47 @@
 // Exit to see the output before mail() attempts to send
 // exit(); // You can uncomment this to stop script execution here
 
-$name = $_POST['name']?? '';
-$email = $_POST['email']?? '';
-$subject = $_POST['subject']?? '';
-$message = $_POST['message']?? '';
+$name = $email = $subject = $message = '';
+
+if(!($_SERVER['REQUEST_METHOD']=='POST')){
+
+}
+else{
+
+    if(empty($_POST['name'])){
+        echo "Input Name<br>";
+    }else{
+        // $name = sanitize($_POST['name'])
+        $name = trim($_POST['name']);
+        $name = stripslashes($_POST['name']);
+        $name = htmlspecialchars($_POST['name']);
+    } if(!preg_match("/^[a-zA-Z-' ]*$/",$name)){
+        echo "Invalid Name<br>";
+    }
+
+    if(empty($_POST['email'])){
+        echo "Required Email<br>";
+    } else{
+        $email = trim($_POST['email']);
+        $email = stripslashes($_POST['email']);
+        $email = htmlspecialchars($_POST['email']);
+    } if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo "Invalid Email<br>";
+    }
+
+    // if(){
+
+    // }else{
+
+    // }
+
+    if(empty($_POST['message'])){
+        echo "Your Message Objective Isn't Mentioned <br>";
+    }else{
+        $message = trim($_POST['message']);
+        $message = stripslashes($_POST['message']);
+        $message = htmlspecialchars($_POST['message']);
+    }
 
 $to = "sexymosquito08@gmail.com";
 $from = $subject;
@@ -20,10 +57,11 @@ $headers = "From: $from\r\nReply-To: $email\r\n";
 $body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
 
 if(mail($to, "New Contact: $from", $body, $headers)){
-    echo "<b>Success!</b> Message sent <br>
-    Go back to <a href='../index.html'>Home Page</a>";
+    header("Location: ../index.html");
+    echo "<script>alert('Mail Sent, We will get back to you');</script>";
+    exit;
 
 } else {
-    echo "Error: Mail could not be sent. Check C:/xampp/sendmail/error.log";
-}
+    echo "Error: Mail could not be sent.";
+}}
 ?>
